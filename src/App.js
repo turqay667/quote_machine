48ef75bf249afcf86c9570bd6444b31c537e908f
Turgay Mammadov <memmedovturqay871@gmail.com>
2023-03-04 16:55:55 +0400

    uploading

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect,useState } from 'react';
+import {FaQuoteLeft } from "react-icons/fa"
 
 function App() {
+  const [quote,setQuote]=useState('')
+  const [author,setAuthor]=useState('')
+ 
+  useEffect(()=>{
+    fetchQuotes()
+    },[])
+
+  const fetchQuotes=async()=>
+    await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json")
+    .then(response=>response.json())
+    .then(data=>{
+      let dataQuotes=data.quotes;
+      let randomIndex=Math.floor(Math.random()*dataQuotes.length)
+      let randomQuote=dataQuotes[randomIndex]
+      setQuote(randomQuote.quote)
+      setAuthor(randomQuote.author)
+    })
+
+
+  const changeQuote=()=>{
+  fetchQuotes()
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+{/* <p>Random quote machine</p> */}
+<div className='quote-box'>
+
+
+<p className='quote-text'>
+<FaQuoteLeft/>
+<span>
+{quote}
+</span>
+
+</p>  
+ <div className='quote-author'>- <span>{author}</span></div>
+<div className='random-quote'><button className='new-quote' onClick={changeQuote}>New quote</button></div>
+</div>
     </div>
+
   );
 }
 
